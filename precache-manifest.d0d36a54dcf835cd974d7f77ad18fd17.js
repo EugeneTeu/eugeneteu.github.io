b931336dd59a5f7aca2ada3ae223482007775835
eugene teu <eugeneteu@gmail.com>
2020-04-05 21:45:51 +0800

    fix

---
+++ precache-manifest.d0d36a54dcf835cd974d7f77ad18fd17.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c3cbc63a7fc235a539af491d69e858c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72af92a6b62d0a27b8e6",
+    "url": "/static/css/main.40bf3cac.chunk.css"
+  },
+  {
+    "revision": "078d29f0259f1661d6d8",
+    "url": "/static/js/0.50c2f6ba.chunk.js"
+  },
+  {
+    "revision": "8ce20c8d3bbf8ac46e7e",
+    "url": "/static/js/1.f8be69f0.chunk.js"
+  },
+  {
+    "revision": "ab1119a94b17f27ed555",
+    "url": "/static/js/4.1107864a.chunk.js"
+  },
+  {
+    "revision": "35ebda3f76cd3aa5d514",
+    "url": "/static/js/5.06a901f5.chunk.js"
+  },
+  {
+    "revision": "72af92a6b62d0a27b8e6",
+    "url": "/static/js/main.24704064.chunk.js"
+  },
+  {
+    "revision": "ea3eb2f3c4ba65c18a41",
+    "url": "/static/js/runtime-main.6a3e42d8.js"
+  },
+  {
+    "revision": "0b4a59602b204440b8c89845eb8f59eb",
+    "url": "/static/media/1.0b4a5960.jpg"
+  },
+  {
+    "revision": "043aa256516844ddf078183c11693844",
+    "url": "/static/media/2.043aa256.jpg"
+  },
+  {
+    "revision": "cb8ac12ea793d5c73b61a013ab62ac43",
+    "url": "/static/media/3.cb8ac12e.jpg"
+  },
+  {
+    "revision": "fa537a487a3f6986a566bcb8333a4e31",
+    "url": "/static/media/4.fa537a48.jpg"
+  },
+  {
+    "revision": "dc7c16bbdbeb457584b486854c1c1470",
+    "url": "/static/media/404.dc7c16bb.png"
+  },
+  {
+    "revision": "a582eae0e49853fad188a6c6d894e6af",
+    "url": "/static/media/5.a582eae0.jpg"
+  },
+  {
+    "revision": "f1054c4fc29dd56e6504fdd20ec34094",
+    "url": "/static/media/6.f1054c4f.jpg"
+  },
+  {
+    "revision": "7f4f2642d1b66d31c9410b9111c23256",
+    "url": "/static/media/dsc_pic.7f4f2642.jpg"
+  },
+  {
+    "revision": "cee1cf17584562371ceda9978f933cae",
+    "url": "/static/media/journalPage2.cee1cf17.png"
+  },
+  {
+    "revision": "1b5f580c397e1ecd6c68c9229e87849e",
+    "url": "/static/media/materialdesignicons-webfont.1b5f580c.woff"
+  },
+  {
+    "revision": "36a9acc6f4b5eb1e300db06a32d6f864",
+    "url": "/static/media/materialdesignicons-webfont.36a9acc6.woff2"
+  },
+  {
+    "revision": "5a8dcd07c28d4b3c332048205f21d032",
+    "url": "/static/media/materialdesignicons-webfont.5a8dcd07.ttf"
+  },
+  {
+    "revision": "cf0152b18aa6a735beaaabdab435e7f2",
+    "url": "/static/media/materialdesignicons-webfont.cf0152b1.eot"
+  },
+  {
+    "revision": "e14bb02a82b03dfb21f67eb3b41b6636",
+    "url": "/static/media/treeSecure.e14bb02a.png"
+  },
+  {
+    "revision": "f2b7ceedd155bc159a86a98c8b0bb83a",
+    "url": "/static/media/vantageSg.f2b7ceed.png"
+  }
+]);
