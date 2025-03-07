cd440e1b203b23026bc487feff1041b210984383
eugeneteu <eugeneteu@gmail.com>
2025-03-08 00:02:15 +0800

    add interval animation for hero logo

--- src/components/HeroLogo.tsx
+++ src/components/HeroLogo.tsx
@@ -6,47 +6,36 @@ export default function HeroLogo(): JSXElement {
   let containerRef: HTMLDivElement | undefined;
   let codeRef: SVGSVGElement | undefined;
   let coffeeRef: SVGSVGElement | undefined;
-  let tween2: gsap.core.Tween | undefined;
-  //   let tl = gsap.timeline({
-  //     repeat: -1,
-  //   });
-  const enterAnimation = () => {
-    if (codeRef && coffeeRef) {
-      tween2 = gsap.to(coffeeRef, {
-        translateX: "200%",
-        duration: 2,
-        ease: "elastic",
-      });
-    }
-  };
-
-  const exitAnimation = () => {
-    if (codeRef && coffeeRef) {
-      //   if (tl) {
-      //     tl.reverse();
-      //   }
-
-      if (tween2) {
-        tween2.reverse();
-      }
-    }
-  };
+  let tl = gsap.timeline({
+    repeat: -1,
+  });
 
   onMount(() => {
-    if (containerRef) {
-      containerRef.addEventListener("mouseenter", () => {
-        enterAnimation();
-      });
-      containerRef.addEventListener("mouseleave", () => {
-        exitAnimation();
-      });
-    }
+    setInterval(() => {
+      if (coffeeRef) {
+        tl.to(coffeeRef, {
+          duration: 1,
+        });
+        tl.to(coffeeRef, {
+          translateX: "200%",
+          duration: 2,
+          ease: "elastic",
+        });
+        tl.to(coffeeRef, {
+          duration: 1,
+        });
+        tl.to(coffeeRef, { translateX: "0%", duration: 2, ease: "power1.out" });
+        tl.to(coffeeRef, {
+          duration: 2,
+        });
+      }
+    }, 1000);
   });
 
   return (
     <div ref={containerRef} class="flex gap-2 w-fit">
       <Bitcoin />
-      <CodeXml ref={codeRef} />
+      <CodeXml />
       <Coffee ref={coffeeRef} />
     </div>
   );
