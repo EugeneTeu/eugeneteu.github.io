0de0cc4a44c38107dce9a49f2830671a37eaba9b
eugeneteu <eugeneteu@gmail.com>
2025-04-01 23:45:22 +0800

    add blog landing page

--- src/routes/blog/(blog).tsx
+++ src/routes/blog/(blog).tsx
@@ -1,27 +1,17 @@
-import { Meta, Title } from "@solidjs/meta";
-import { RouteSectionProps } from "@solidjs/router";
-//@ts-expect-error
-import { MDXProvider } from "solid-mdx";
-import { markdownComponents } from "~/components/Markdown";
-import { PostMetaData, posts } from "~/data/posts";
-import { ArrowLeft, Construction, MoveUpLeftIcon } from "lucide-solid";
+import { Construction } from "lucide-solid";
 import { JSXElement } from "solid-js";
+import BentoBox from "~/components/BentoBox";
+import { Blog } from "~/content/content";
 
-export default function Blog(): JSXElement {
+export default function BlogHome(): JSXElement {
+  const blogSection = <BentoBox>{Blog()}</BentoBox>;
   return (
     <>
       <main class="bg-white dark:bg-gray-900 pb-safe min-h-screen overflow-auto mb-10">
         <div
           class={`max-w-5xl mx-5 sm:mx-10 md:mx-auto md:w-fit lg:mx-auto pt-4 pb-4`}
         >
-          <div class="flex flex-row w-100">
-            <div class="mx-3">
-              <Construction />
-            </div>
-            <div>
-              <h1>Sorry still under construction</h1>
-            </div>
-          </div>
+          {blogSection}
         </div>
       </main>
     </>
